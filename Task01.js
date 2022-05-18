6195f4727aa386372cb54fd6192e71edc9439aef
SyedaRubaishaAli <105500769+SyedaRubaishaAli@users.noreply.github.com>
2022-05-18 17:39:21 +0400

    Add files via upload

---
+++ Task01.js
@@ -0,0 +1,28 @@
+// Task 01
+
+studentData = ["Rubaisha","BCP027","Blockchain",true,500]
+
+function ShowData() {
+    console.log(studentData);
+}
+ShowData()
+
+// Push:to insert Something in the Array at the end
+
+studentData.push("Syeda")
+ShowData()
+
+// Pop:to remove something in the Array at the end
+
+studentData.pop("Syeda")
+ShowData()
+
+// Unshift:to insert something in the Array at first
+
+studentData.unshift("Usman")
+ShowData()
+
+// Shift: to remove something in the Array at first
+
+studentData.shift("Usman")
+ShowData()
